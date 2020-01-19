3015f852359a138ef8bfe28c32d93814528dbd7d
mohanor <mohanoratef6@gmail.com>
2020-01-19 16:56:54 +0100

    v1.0

--- js/main.js
+++ js/main.js
@@ -5,7 +5,9 @@ btn.addEventListener('click', ()=>{
     if( more.style.display == "block" )
     {
         more.style.display = "none";
+        btn.innerHTML ="READ MORE";
     }else{
         more.style.display = "block";
+        btn.innerHTML = "READ LESS";
     }
 });
